1be911458892e86ec2a27af4c0a127f4218d24be
Shaima Ahmed <shaimatasneemahmed@gmail.com>
2023-11-20 16:45:45 +0530

    Add code.py

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Web-Scraping-and-Sentiment-Analysis-Tool
+# Web Scraping and Sentiment Analysis Tool
+
+This Python script uses web scraping techniques and sentiment analysis to extract and analyze the content of articles from specified URLs. It uses the requests library to fetch HTML content, BeautifulSoup for parsing, and various Natural Language Processing (NLP) techniques to analyze sentiment.
+
+Features:
+1. Input Data: Reads input data from an Excel file (Input.xlsx) containing URLs to articles.
+2. Web Scraping: Retrieves the HTML content of each article from the provided URLs using the requests library.
+3. Text Extraction: Extracts the title and content of each article using BeautifulSoup's HTML parsing capabilities.
+4. Stop Words Removal: Utilizes a stop words directory to exclude common words from the article text, enhancing sentiment analysis accuracy.
+5. Sentiment Analysis: Calculates negative, positive, polarity, and subjectivity scores for each article.
+6. Output: Outputs results, including excluded stop words and sentiment scores, to the console.
+
+Dependencies:
+1. requests: For making HTTP requests to fetch HTML content.
+2. BeautifulSoup: For parsing HTML content.
+3. pandas: For reading input data from an Excel file.
+4. nltk: For Natural Language Processing tasks, including tokenization.
---
+++ code.py
@@ -0,0 +1,82 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+import os
+import nltk 
+
+nltk.download()
+
+def read_input(xlsx_file):
+    return pd.read_excel(xlsx_file, header=0, dtype=str)
+        
+def url_content(url):
+    response=requests.get(url)
+    if response.status_code==200:
+        url_content=response.text
+        return url_content
+    else:
+        return ("Failed to get the url content")
+
+def extract_article_content(file):
+    title= file.find("h1")
+    content = file.find('div',
+             {'class': lambda x: x 
+                       and "td-post-content" in x.split()
+             })
+    return title.text, content.text
+
+def stop_words(article_text, stop_dir):
+    stop_files= os.listdir(stop_dir)
+    for s_file in stop_files:
+        read_file=pd.read_csv(s_file,header=None,usecols=0)
+        for word in read_file:
+            word.split(()[0])
+            no_stop=re.sub(f"{word}","", article_text)
+    return no_stop
+
+def positive_negative_score(file_path_nw,file_path_pw, article_text_exclude_stop):
+    with open(file_path_nw) as nw:
+            words= nltk.word_tokenize(article_text_exclude_stop)
+            intersection_n= len(set(nw) & set(words))
+    with open(file_path_pw) as nw:
+            words= nltk.word_tokenize(article_text_exclude_stop)
+            intersection_p= len(set(nw) & set(words))
+    return intersection_n, intersection_p
+
+def polarity_score(negative_score, positive_score):
+    den=(positive_score + negative_score + 0.000001)
+    num= positive_score-negative_score
+    value_ps=  num / den
+    return value_ps
+
+def subjectivity_score(negative_score, positive_score, article_text_exclude_stop):
+    total_words= len(nltk.word_tokenize(article_text_exclude_stop))
+    den=total_words + 0.000001
+    num= positive_score-negative_score
+    value_ss= num/den
+    return value_ss
+                                   
+def main():
+
+    xlsx_df=read_input("Input.xlsx")
+    for index, row in xlsx_df.iterrows():
+            print(row)
+            html_content=url_content(row['URL'])
+            file= BeautifulSoup(html_content, "html.parser")
+            article_title, article_text= extract_article_content(file)
+            # what to do in case of Error:404 page not found
+            with open(f"ArticleFiles/{row['URL_ID']}.txt", "w", encoding="utf-8") as url_file:
+                url_file.write(f'{article_title}\n')
+                url_file.write(article_text)
+            article_text_exclude_stop= stop_words(article_text,"StopWords")
+            negative_score, positive_score= positive_negative_score("MasterDictionary/negative-words.txt", "MasterDictionary/positive-words.txt", article_text_exclude_stop ) 
+            polarity_score= polarity_score(negative_score,positive_score )
+            subjectivity_score= subjectivity_score(negative_score,positive_score,article_text_exclude_stop)      
+            print("Excluded Stop Words from Article Text:", article_text_exclude_stop)
+            print("Negative Score:", negative_score)
+            print("Positive Score:", positive_score)
+            print("Polarity Score:", polarity_score)
+            print("Subjectivity Score:", subjectivity_score)       
+if __name__ == '__main__':
+    main()
